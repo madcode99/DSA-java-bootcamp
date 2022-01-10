89f521a6c8a4e061d70ed0042c639783d686301f
Sara Hassan <92973686+madcode99@users.noreply.github.com>
2022-01-10 14:58:01 +0430

    Add files via upload

---
+++ bootcamp-second-assignment/Question2.java
@@ -0,0 +1,11 @@
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        //Take name as input and print a greeting message for that name.
+        System.out.println("Enter your name :");
+        String name=input.next();
+        System.out.println(" Hello "+name+" how are you ?");
+
+    }
+    
+}
