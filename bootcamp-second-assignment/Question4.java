2db97379d97ce9c81f3dbc7fd7b5f350edf206f6
Sara Hassan <92973686+madcode99@users.noreply.github.com>
2022-01-11 16:11:20 +0430

    Add files via upload

---
+++ bootcamp-second-assignment/Question4.java
@@ -0,0 +1,23 @@
+public class Question4{
+     public static void main(String[] args) {
+        //Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter a number :");
+        int num1 = input.nextInt();
+        System.out.print("Enter any of the following (+, -, *, /) operators :");
+        char op = input.next().charAt(0);
+        System.out.print("Enter another number :");
+        int num2 =input.nextInt();
+        if (op == '+'){
+            System.out.println(num1 +" + "+num2+" = "+num1+num2);
+        }else if(op == '-'){
+            System.out.println(num1 +" - "+num2+" = " +(num1 - num2));
+        }else if(op == '*'){
+            System.out.println(num1 +" * "+num2+" = "+num1*num2);
+        }else if(op == '/'){
+            System.out.println(num1 +" / "+num2+" = "+num1/num2);
+        }else{
+            System.out.println(" invalid operator ");
+        }
+    }
+}
---
+++ bootcamp-second-assignment/Question5.java
@@ -0,0 +1,18 @@
+public class Question5{
+        public static void main(String[] args) {
+        //Take 2 numbers as input and print the largest number.
+        Scanner input =new Scanner(System.in);
+        System.out.print("Enter two numbers :");
+
+        int num= input.nextInt();
+        int num1= input.nextInt();
+        int LargestNum =0;
+
+      if(num > num1){
+          LargestNum=num;
+      }else if(num1 > num){
+          LargestNum=num;
+      }
+        System.out.println("The Largest number is  :"+LargestNum);
+    }
+}
