9a72b50292f97b74d028bfe6ccb500fa88825db3
Sara Hassan <92973686+madcode99@users.noreply.github.com>
2022-01-13 14:40:46 +0430

    Add files via upload

---
+++ bootcamp-second-assignment/Question8.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Question8 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        //To find out whether the given String is Palindrome or not.
+        System.out.print("Enter a string : ");
+        String text = input.next();
+        String Rev = "";
+
+        for(int i=text.length()-1;i>=0;i--){
+
+            Rev=Rev+text.charAt(i);
+
+        }
+        System.out.println(Rev);
+        if(text.equals(Rev)){
+            System.out.println("it is palindrome");
+        }else {
+            System.out.println("it is not a palindrome");
+        }
+
+    }
+}
---
+++ bootcamp-second-assignment/Question9.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Question9 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        //To find Armstrong Number between two given number.
+        int a=input.nextInt();
+        int b=input.nextInt();
+        for(int i=a;i<=b;i++){
+            int num=i;
+            int sum=0;
+            while(num>0){
+                int rem=num%10;
+                sum=sum+(rem*rem*rem);
+                num=num/10;
+
+            }
+            if (sum == i){
+                System.out.println(sum);
+            }
+        }
+
+    }
+}
