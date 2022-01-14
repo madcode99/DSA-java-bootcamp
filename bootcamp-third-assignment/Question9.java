970c5006d471b17b36416d5205c8c304fca31eab
Sara Hassan <92973686+madcode99@users.noreply.github.com>
2022-01-14 13:40:29 +0430

    Add files via upload

---
+++ bootcamp-third-assignment/Question9.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Question9 {
+    public static void main(String[] args) {
+        //
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the value of a :");
+        float a =input.nextFloat();
+        float P=3*a;
+        System.out.println("The Perimeter Of Equilateral Triangle is : "+P);
+    }
+}
