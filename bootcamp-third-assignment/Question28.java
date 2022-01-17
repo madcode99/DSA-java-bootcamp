2be5ab9ba6c333a19bdc2dc50fba04aeb7c5c192
Sara Hassan <92973686+madcode99@users.noreply.github.com>
2022-01-17 16:50:35 +0430

    Add files via upload

---
+++ bootcamp-third-assignment/Question28.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Question28 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Calculate Average Of N Numbers
+        int n=0;
+        int sum=0;
+        int count =0;
+        float Average=0;
+        while(true){
+            System.out.print("Enter a number : ");
+             n=input.nextInt();
+            sum+=n;
+            count++;
+            if(n==0){
+                count--;
+                break;
+            }
+        }
+        Average=sum/count;
+        System.out.print("The average of given values is : "+Average);
+    }
+}
