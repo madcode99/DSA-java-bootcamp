d92fe91d7afb6ae55c663a7d2322fc03c518f4dc
Sara Hassan <92973686+madcode99@users.noreply.github.com>
2022-05-02 15:56:41 +0430

    Add files via upload

---
+++ bootcamp-second-assignment/FindSimpleInterest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FindSimpleInterest {
+    public static void main(String[] args) {
+        //Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter Principal amount :");
+        float P=input.nextFloat();
+        System.out.print(" Enter time  :");
+        float T= input.nextFloat();
+        System.out.print("Enter rate :");
+        float  R =input.nextFloat();
+        // interest formula (P * R * T)/100 =Interest
+        float Interest= (P * R * T)/100;
+        System.out.print("your Interest will be  :  "+Interest);
+    }
+}
