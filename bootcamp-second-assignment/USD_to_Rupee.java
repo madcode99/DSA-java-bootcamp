5b5572cefe9ff161c7c2b405f9df6da0e33e1dbc
Sara Hassan <92973686+madcode99@users.noreply.github.com>
2022-05-04 16:01:10 +0430

    Add files via upload

---
+++ bootcamp-second-assignment/USD_to_Rupee.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class USD_to_Rupee {
+    public static void main(String[] args) {
+        //Input currency in rupees and output in USD
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter rupee : ");
+        float Rupees = input.nextFloat();
+        float USD = (float)(0.014);
+        for(int i=2;i<=Rupees;i++){
+            USD += 0.014;
+        }
+        System.out.println("The USD will be :"+USD);
+    }
+}
