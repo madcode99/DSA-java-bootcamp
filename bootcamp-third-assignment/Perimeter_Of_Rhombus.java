149f7fc894f4a4d49240e35f23126efdc1aa7bfb
Sara Hassan <92973686+madcode99@users.noreply.github.com>
2022-05-16 20:15:54 +0430

    Add files via upload

---
+++ bootcamp-third-assignment/Perimeter_Of_Rhombus.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Perimeter_Of_Rhombus {
+    public static void main(String[] args) {
+        //Perimeter Of Rhombus
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the value of a : ");
+        float a=input.nextFloat();
+        float P=4*a;
+        System.out.println(" The Perimeter Of Rhombus is : "+P);
+
+    }
+}
