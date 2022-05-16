88c2788563feebd2603a3563d1583e9b31949df9
Sara Hassan <92973686+madcode99@users.noreply.github.com>
2022-05-16 20:13:59 +0430

    Add files via upload

---
+++ bootcamp-third-assignment/Perimeter_Of_Square.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Perimeter_Of_Square {
+    public static void main(String[] args) {
+        //Perimeter Of Square
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the value of a :");
+        float a=input.nextFloat();
+        float P=4*a;
+        System.out.println("The Perimeter Of Square is : "+P);
+    }
+}
