4557186c4cb34f2b6b771e7e2ba16cff380825f8
Sara Hassan <92973686+madcode99@users.noreply.github.com>
2022-06-23 20:03:17 +0430

    Add files via upload

---
+++ bootcamp-third-assignment/Factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        // Factorial Program In Java
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num=input.nextInt();
+        int fact=1;
+        for(int i=1;i<=num;i++){
+            fact=fact*i;
+        }
+        System.out.println("the factorial of "+num+" is "+fact);
+    }
+}
